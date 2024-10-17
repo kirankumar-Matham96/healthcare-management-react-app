abf9e8e9364aab21b6f6c83e81088fe9f3367296
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-10-17 20:18:43 +0530

    feat: added feedback section

--- src/components/CarouselComponent/index.jsx
+++ src/components/CarouselComponent/index.jsx
@@ -4,16 +4,34 @@ import carouselStyles from "./index.module.css";
 
 export const CarouselComponent = (props) => (
   <Carousel fade controls={false} indicators={false} interval={3000}>
-    {props.imageSet.map((image) => (
-      <Carousel.Item>
-        <img key={image.title} src={image.url} alt={image.title} />
-        <Carousel.Caption>
-          <div className={carouselStyles.blurText}>
-            <h3 className="text-shadow">{image.title}</h3>
-            <p>{image.caption}</p>
-          </div>
-        </Carousel.Caption>
-      </Carousel.Item>
-    ))}
+    {props.imageSet
+      ? props.imageSet.map((image) => (
+          <Carousel.Item>
+            <img key={image.title} src={image.url} alt={image.title} />
+            <Carousel.Caption>
+              <div className={carouselStyles.blurText}>
+                <h3 className="text-shadow">{image.title}</h3>
+                <p>{image.caption}</p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))
+      : props.feedback.map((item) => (
+          <Carousel.Item>
+            <img
+              className={carouselStyles.transparentImage}
+              key={item.patient}
+              src="https://res.cloudinary.com/do4v7miwh/image/upload/v1729175404/samples/Backgrounds/chris-appano-F59muNgxqlg-unsplash_pd4sb3.jpg"
+              alt="bg"
+            />
+            <Carousel.Caption>
+              <div className={carouselStyles.textContent}>
+                <p className="text-shadow">{item.message}</p>
+                <br />
+                <h6>~ {item.patient}</h6>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
   </Carousel>
 );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -27,6 +27,34 @@ export const Home = () => {
     },
   ];
 
+  const feedback = [
+    {
+      message:
+        "From the moment I arrived, I was treated with kindness and respect. The staff made sure I was comfortable and answered all of my questions patiently. The facilities were top-notch, and I felt that the entire team was genuinely concerned about my well-being. I couldn’t have asked for better care during my hospital stay.",
+      patient: "John Doe",
+    },
+    {
+      message:
+        "The care I received at the hospital was beyond my expectations. The medical team was highly professional, and they made sure to keep me informed at every stage of my surgery. The nurses were attentive and always ready to help, no matter the time of day. The room was clean and quiet, which helped me recover faster.",
+      patient: "Alice",
+    },
+    {
+      message:
+        "I had a very positive experience throughout my treatment. The doctors and nurses were not only skilled but also compassionate. They took the time to explain my diagnosis and treatment options in detail, which helped ease my anxiety. The hospital facilities were modern and well-maintained.",
+      patient: "Jake",
+    },
+    {
+      message:
+        "My surgery and recovery process went smoother than I could have imagined. The surgical team was incredibly professional, and I felt safe and reassured in their care. The post-op care was also remarkable. The nurses were always available and made sure I was comfortable and recovering well.",
+      patient: "Malini",
+    },
+    {
+      message:
+        "The entire experience at this hospital was outstanding. From the moment I checked in, I felt like I was in good hands. The staff was incredibly welcoming, and the doctors took the time to thoroughly explain my treatment plan. The patient rooms were spacious and equipped with everything I needed for a comfortable stay. ",
+      patient: "Vivek",
+    },
+  ];
+
   return (
     <div className={homeStyles.bgContainer}>
       <header className={homeStyles.hero}>
@@ -75,7 +103,10 @@ export const Home = () => {
           <CarouselComponent imageSet={imageSet} />
         </div>
       </section>
-      <section className={homeStyles.feedback}></section>
+      <section className={homeStyles.feedback}>
+        <h2 className={homeStyles.h2}>What Our Patients Say</h2>
+        <CarouselComponent feedback={feedback} />
+      </section>
       <section className={homeStyles.contacts}></section>
       <footer className={homeStyles.footer}></footer>
     </div>
